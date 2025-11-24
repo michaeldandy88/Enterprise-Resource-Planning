467ab7ce32031d63ff2f88606ec3e947c4142a60
michaeldandy88 <dandymchl31@gmail.com>
2025-11-24 12:35:36 +0700

    Setup

---
+++ resources/js/components/App.vue
@@ -0,0 +1,4 @@
+<template>
+    <img src="https://media.tenor.com/3ydOlynD-2EAAAAM/dance-dance-boy.gif" alt="Party🥳"></img>
+    <h1><b>alek bijer</b></h1>
+</template>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Laravel + Vue</title>
+        @vite(['resources/js/app.js', 'resources/css/app.css'])
+    </head>
+
+    <body>
+        <div id="app"></div>
+    </body>
+</html>
